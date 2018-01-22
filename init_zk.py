dd3e553c600df0c8404b33c9686ee80b28470793
nbhoa <asc686f61@gmail.com>
2018-01-22 09:25:55 +0700

    test 1

--- init_zk.py
+++ init_zk.py
@@ -13,6 +13,7 @@ def set(path, data):
         zk.delete(path)
     zk.create(path, json.dumps(data, indent=4).encode(), makepath=True)
 
+#test amend 1
 
 set('/{}/fxservice'.format(root_path),
     {
