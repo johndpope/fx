7c3ea87c10eb27fc6653eb60d2e0c4be1f1e7a9b
nbhoa <asc686f61@gmail.com>
2018-01-26 17:24:31 +0700

    cho strategy vào docker

--- strategy/strategy/upload.py
+++ strategy/strategy/upload.py
@@ -10,8 +10,10 @@ zk.start()
 monitor_path = os.path.join(os.environ['ZkBasePath'], "tasks")
 
 for v in [x for x in os.listdir(os.path.dirname(__file__)) if x.endswith(".zip")]:
-    with open(v, 'rb') as f:
-        full_path = os.path.join(monitor_path, v)
-        if zk.exists(full_path):
-            continue
-        zk.create(full_path, f.read(), ephemeral=False, makepath=True)
+    for l in range(0,200):
+        with open(v, 'rb') as f:
+            full_path = os.path.join(monitor_path, v) + '_' + str(l)
+            if zk.exists(full_path):
+                continue
+            print('upload:' + full_path)
+            zk.create(full_path, f.read(), ephemeral=False, makepath=True)
